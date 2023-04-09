6a8b014183a05e2f67399d500eb9c776fbea78da
Bsodoge <kashhh4u@gmail.com>
2023-04-09 12:55:13 +0100

    Add ability to increment the percentage

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 let percentage = document.getElementById('percentage');
+let progress = 0;
+setInterval(() => {
+    if(progress !== 99){
+        progress++
+        percentage.innerText = progress;
+    }
+}, Math.floor(Math.random() * 50000));
